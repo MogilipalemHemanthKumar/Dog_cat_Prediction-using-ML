e9c6a880a824bbb991d7cf6d5968eed5188c4d40
MogilipalemHemanthKumar <107172150+MogilipalemHemanthKumar@users.noreply.github.com>
2022-12-22 01:01:52 +0530

    Add files via upload

---
+++ dog_cat_ML.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import cv2
+import numpy as np
+import tensorflow_hub as hub
+mobilenet_model = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
+path = 'dog_cat.h5'
+data= tf.keras.models.load_model(path ,custom_objects={'KerasLayer': hub.KerasLayer})
+img_path=st.text_input("Enter the path of the image: ")
+img=Image.open(img_path)
+st.image(img, caption='Image of given Path')
+if(st.button("Predict Result")):
+
+    input_image = cv2.imread(img_path)
+    input_image_resize = cv2.resize(input_image, (224, 224))
+    input_image_scaled = input_image_resize / 255
+
+    image_reshaped = np.reshape(input_image_scaled, [1, 224, 224, 3])
+
+    input_prediction = data.predict(image_reshaped)
+
+
+    input_pred_label = np.argmax(input_prediction)
+
+
+    if input_pred_label == 0:
+        st.success('The image represents a Cat')
+
+    else:
+        st.success('The image represents a Dog')
